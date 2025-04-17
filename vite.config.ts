29d4ec58093b5dd5f2401b84a9cd17567ea98445
stephane <stephaneadeso@gmail.com>
2025-04-17 19:21:37 +0200

    feat: add path alias for src

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,33 @@
-import react from "@vitejs/plugin-react";
-import { dirname, resolve } from "node:path";
-import { fileURLToPath } from "node:url";
-import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
+import react from '@vitejs/plugin-react';
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), dts({ rollupTypes: true })],
-    build: {
-        lib: {
-            entry: resolve(__dirname, "src/main.ts"),
-            name: "cb-react-components",
-            fileName: "cb-react-components"
+  plugins: [react(), dts({ rollupTypes: true })],
+  resolve: {
+    alias: {
+      '@src': resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      name: 'cb-react-components',
+      fileName: 'cb-react-components',
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDom',
+          'react/jsx-runtime': 'react/jsx-runtime',
         },
-        rollupOptions: {
-            external: ["react", "react-dom", "react/jsx-runtime"],
-            output: {
-                globals: {
-                    react: "React",
-                    "react-dom": "ReactDom",
-                    "react/jsx-runtime": "react/jsx-runtime"
-                }
-            }
-        }
-    }
+      },
+    },
+  },
 });
