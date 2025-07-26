9f839dfedc54c83a61d160ceddf0756442786e9d
stephane <stephaneadeso@gmail.com>
2025-07-26 10:10:27 +0200

    feat: add Toast interface to model for enhanced toast notifications

--- src/models/toast/toast.model.ts
+++ src/models/toast/toast.model.ts
@@ -1,1 +1,21 @@
 // -------------- TOAST ITEM----------------------------
+import {
+  BackgroundVariant,
+  Sizes,
+  Variant,
+} from '@src/models/global/Global.model';
+
+export interface Toast {
+  id: string;
+  message: string;
+  title?: string;
+  variant?: Variant;
+  backgroundVariant?: BackgroundVariant;
+  icon?: React.ReactNode;
+  duration?: number;
+  persistant?: boolean;
+  showIcon?: boolean;
+  showProgressBar?: boolean;
+  size?: Sizes;
+  onClose?: (id: string) => void;
+}
